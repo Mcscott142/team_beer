d9dfdac25b5ff7e17f6cce6005da900d183b035b
Colin Scott <m.colin.scott@gmail.com>
2014-06-23 17:31:57 -0400

    User signin/signout/creation test written

--- Gemfile
+++ Gemfile
@@ -22,9 +22,17 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
-
+gem 'foundation-rails'
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-gem 'spring',        group: :development
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'factory_girl'
+  gem 'spring'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'launchy'
+end
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
---
+++ spec/features/user_signin_account_creation_spec.rb
@@ -0,0 +1,57 @@
+require 'rails-helper'
+
+feature 'user creates an account, signs in, signs out', %Q{
+  # -As a user I would like to create an account
+  # -As a user I would like to sign in
+  # -As a user I would like to sign out
+} do
+
+  scenario '-user can create an account' do
+    visit "/"
+    click_on "Signup!"
+    fill_in 'Email' with 'test@test.com'
+    fill_in 'Password' with '1234abcd'
+    fill_in 'Username' with 'Will1492'
+    click_on 'Over 21?'
+    click_on 'Submit'
+
+    expect(page).to have_content 'Will1492'
+    expect(page).to have_content 'Sign out'
+    expect(page).to have_content 'Beer'
+  end
+
+  scenario '-user can sign in to their account' do
+    visit "/"
+    click_on "Signup!"
+    fill_in 'Email' with 'test@test.com'
+    fill_in 'Password' with '1234abcd'
+    fill_in 'Username' with 'Will1492'
+    click_on 'Over 21?'
+    click_on 'Submit'
+
+    click_on 'Sign out'
+    expect(page).to have_content 'Sign in'
+    click_on 'Sign in'
+    fill_in 'Email' with 'test@test.com'
+    fill_in 'Password' with '1234abcd'
+    click_on 'Submit'
+    expect(page).to have_content 'Will1492'
+    expect(page).to have_content 'Sign out'
+  end
+
+  scenario '-user can sign out of their account' do
+    visit "/"
+    click_on "Signup!"
+    fill_in 'Email' with 'test@test.com'
+    fill_in 'Password' with '1234abcd'
+    fill_in 'Username' with 'Will1492'
+    click_on 'Over 21?'
+    click_on 'Submit'
+
+    click_on 'Sign out'
+    expect(page).to have_content 'Sign in'
+    expect(page).to_not have_content 'Will1492'
+    expect(page).to_not have_content 'Sign out'
+  end
+end
+
