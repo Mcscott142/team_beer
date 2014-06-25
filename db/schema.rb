c793d1bd917a0ad0a56f4c2a1da068cc6191d657
c13iscuit <barnett.chri@gmail.com>
2014-06-25 10:30:14 -0400

    Added indexes to foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625114832) do
+ActiveRecord::Schema.define(version: 20140625142226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20140625114832) do
     t.integer  "beer_type_id",    null: false
   end
 
+  add_index "beers", ["beer_type_id"], name: "index_beers_on_beer_type_id", using: :btree
+  add_index "beers", ["brewery_id"], name: "index_beers_on_brewery_id", using: :btree
+
   create_table "breweries", force: true do |t|
     t.string   "name",        null: false
     t.string   "description", null: false
@@ -44,6 +47,8 @@ ActiveRecord::Schema.define(version: 20140625114832) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "breweries", ["region_id"], name: "index_breweries_on_region_id", using: :btree
 
   create_table "regions", force: true do |t|
     t.string   "name",       null: false
@@ -60,6 +65,9 @@ ActiveRecord::Schema.define(version: 20140625114832) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "reviews", ["beer_id"], name: "index_reviews_on_beer_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -91,5 +99,8 @@ ActiveRecord::Schema.define(version: 20140625114832) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
+  add_index "votes", ["voteable_id", "voteable_type"], name: "index_votes_on_voteable_id_and_voteable_type", using: :btree
 
 end
