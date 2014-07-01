1671436b8eb11ecbdfa568debd7f185df74bbf9d
Faizaan Shamsi <faizaan.shamsi@launchacademy.com>
2014-07-01 14:32:20 -0400

    Update votes_controller.rb

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,11 +11,11 @@ class VotesController < ApplicationController
         if existing_vote.vote.to_i == -1 && params[:vote].to_i == 1
           existing_vote.vote = 1
           existing_vote.save
-          2.times {existing_vote.vote_on_this_object}
+          2.times { existing_vote.vote_on_this_object }
         elsif existing_vote.vote.to_i == 1 && params[:vote].to_i == -1
           existing_vote.vote = -1
           existing_vote.save
-          2.times {existing_vote.vote_on_this_object}
+          2.times { existing_vote.vote_on_this_object }
         elsif existing_vote.vote.to_i == params[:vote].to_i
           flash[:notice] = "You already voted on that!"
         end
